56730e468fd5b419101dcad14b0fb03448331403
Jay Millena <jay.millena07@gmail.com>
2024-03-05 16:17:42 +0800

    Reused the form by using isset command

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,39 +1,5 @@
 @extends('layouts.app')
 
-@section('title', 'Edit Task')
-
 @section('content')
-
-  <form method="POST" action="{{ route('task.update', ['task' => $task]) }}">
-    @csrf
-    @method('PUT')
-    <div>
-      <label for="title">Title</label>
-      <input type="text" name="title" id="title" value="{{ $task->title }}"/>
-      @error('title')
-        <p>{{ $message }}</p>
-      @enderror
-    </div>
-
-    <div>
-      <label for="description">Description</label>
-      <textarea name="description" id="description" rows="5">{{ $task->description }}</textarea>
-      @error('description')
-        <p>{{ $message }}</p>
-      @enderror
-    </div>
-
-    <div> 
-      <label for="long_description">Long Description</label>
-      <textarea name="long_description" id="long_description" rows="10">{{ $task->long_description }}</textarea>
-      @error('long_description')
-        <p>{{ $message }}</p>
-      @enderror
-    </div>
-    
-    <div>
-      <button type="submit">Update Task</button>
-    </div>
-  </form>
-
+  @include('form')
 @endsection
