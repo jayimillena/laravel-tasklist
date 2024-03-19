d36d63a6d2e535d568cf7787faa82b52394003f3
Jay Millena <jay.millena07@gmail.com>
2024-03-19 12:11:29 +0800

    Styling form using tailwindcss

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -9,32 +9,32 @@
     action="{{ isset($task) ? route('task.update', ['task' => $task]) : route('task.store') }}">
     @csrf
     @isset($task) @method('PUT') @endisset
-    <div>
-      <label class="label" for="title">Title</label>
-      <input type="text" name="title" id="title"  value="{{ $task->title ?? old('title') }}"/>
+    <div class="mb-4">
+      <label class="block uppercase text-slate-700 mb-2" for="title">Title</label>
+      <input class="shadow-sm apperance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none" type="text" name="title" id="title"  value="{{ $task->title ?? old('title') }}"/>
       @error('title')
         <p>{{ $message }}</p>
       @enderror
     </div>
 
-    <div>
-      <label class="label" for="description">Description</label>
-      <textarea name="description" id="description" rows="5">{{ $task->description ?? old('description') }}</textarea>
+    <div class="mb-4">
+      <label class="block uppercase text-slate-700 mb-2" for="description">Description</label>
+      <textarea class="shadow-sm apperance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none" name="description" id="description" rows="5">{{ $task->description ?? old('description') }}</textarea>
       @error('description')
         <p>{{ $message }}</p>
       @enderror
     </div>
 
     <div> 
-      <label class="label" for="long_description">Long Description</label>
-      <textarea name="long_description" id="long_description" rows="10">{{ $task->long_description ?? old('long_description') }}</textarea>
+      <label class="block uppercase text-slate-700 mb-2" for="long_description">Long Description</label>
+      <textarea class="shadow-sm apperance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none" name="long_description" id="long_description" rows="10">{{ $task->long_description ?? old('long_description') }}</textarea>
       @error('long_description')
         <p>{{ $message }}</p>
       @enderror
     </div>
     
-    <div>
-      <button class="" type="submit">
+    <div class="mb-4">
+      <button class="rounded-md px-2 py-1 text-center font-medium shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50" type="submit">
         @isset($task)
           Update Task
         @else 
